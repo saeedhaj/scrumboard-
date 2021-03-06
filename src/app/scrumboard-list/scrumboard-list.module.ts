154de51c1a2acfdf2b8e6592a2fe5c164a40b668
saeedhaj <saeed.hajiaghzadeh@gmail.com>
2021-03-06 14:31:50 +0330

    style to component scrumboard-list

--- src/app/scrumboard-list/scrumboard-list.module.ts
+++ src/app/scrumboard-list/scrumboard-list.module.ts
@@ -4,12 +4,32 @@ import { CommonModule } from '@angular/common';
 import { ScrumboardListRoutingModule } from './scrumboard-list-routing.module';
 import { ScrumboardListComponent } from './scrumboard-list.component';
 
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCardModule } from "@angular/material/card";
+import { MatDialogModule } from "@angular/material/dialog";
+
+const MATERIAL_MODULES = [
+  
+  MatButtonModule,
+  MatIconModule,
+  MatInputModule,
+  MatSnackBarModule,
+  MatCardModule,
+  MatDialogModule
+];
+
 
 @NgModule({
   declarations: [ScrumboardListComponent],
   imports: [
     CommonModule,
-    ScrumboardListRoutingModule
+    ScrumboardListRoutingModule,
+    FlexLayoutModule,
+    ...MATERIAL_MODULES 
   ],
   exports: [ScrumboardListComponent]
 })
